d9952e1d977ae40e701f3020ee9e5269dea79d03
JF-Lalonde <lalondej88@gmail.com>
2017-10-23 10:08:22 -0600

    Completed user story 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
+const $ = require('jquery')
+
 document.addEventListener("DOMContentLoaded", () => {
-  // have fun!
+  topWord()
+  $("button").on("click", textCount)
 })
 
+function topWord(){
+  $.get("https://wordwatch-api.herokuapp.com/api/v1/top_word", function(data) {
+    let word = Object.keys(data.word)
+    let count = Object.values(data.word)
+    $("h3").append(`${word} (${count})`)
+  })
+}
+
+function textCount() {
+  let wordsToCount = $("textarea").val()
+  $.ajax({
+    type: "POST",
+    url: "https://wordwatch-api.herokuapp.com/api/v1/words",
+    data: {word: { value: wordsToCount}},
+  })
+  .done(function() {
+  })
+  .fail(function() {
+    alert("problem!")
+  })
+}
